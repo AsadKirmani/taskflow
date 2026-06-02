20fb7c9dabd7e62bd1eccfeca4011297d78435b5
AsadKirmani <asadkirmani619@gmail.com>
2026-06-02 20:53:29 +0530

    cors fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000/api/v1'
+  apiUrl: 'https://taskflow-backend-olive.vercel.app/api/v1'
 };
--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:4200'],
+    origin: ['http://localhost:4200', 'https://taskflow-sandy-nine.vercel.app'],
     credentials: true
   })
 );
