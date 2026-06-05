30b742cca166b06b8552d9d5eab32138ffd38ca2
AsadKirmani <32667125+AsadKirmani@users.noreply.github.com>
2026-06-05 09:34:09 +0530

    Update app.initializer.ts

--- frontend/src/app/core/config/app.initializer.ts
+++ frontend/src/app/core/config/app.initializer.ts
@@ -1,9 +1,8 @@
 import { inject } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
 import { AuthStoreService } from '../../features/auth/data-access/auth-store.service';
 
 export function initializeApp(): Promise<unknown> {
   const authStore = inject(AuthStoreService);
 
-  return firstValueFrom(authStore.initializeSession());
+  return () => authStore.initializeSession();
 }
