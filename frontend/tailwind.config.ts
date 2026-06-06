aa6e176a14afe3bd3632521bb16d33b83346a950
AsadKirmani <32667125+AsadKirmani@users.noreply.github.com>
2026-06-06 22:15:36 +0530

    Create tailwind.config.ts

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        base: 'var(--color-base)',
+        surface: 'var(--color-surface)',
+        content: {
+          DEFAULT: 'var(--color-content)',
+          muted: 'var(--color-content-muted)'
+        },
+        primary: {
+          DEFAULT: 'var(--color-primary)',
+          content: 'var(--color-primary-content)'
+        },
+        error: 'var(--color-error)',
+        success: 'var(--color-success)'
+      }
+    },
+  },
+  plugins: [],
+}
