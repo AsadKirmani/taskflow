d3151f048d6ed46b9423f4bc8c2915098531746b
AsadKirmani <asadkirmani619@gmail.com>
2026-06-30 12:50:31 +0530

    minor fix

--- backend/src/shared/types/express.d.ts
+++ backend/src/shared/types/express.d.ts
@@ -1,10 +1,12 @@
-import { Request } from 'express';
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    auth?: {
-      userId: string;
-      email: string;
-    };
+declare global {
+  namespace Express {
+    interface Request {
+      auth?: {
+        userId: string;
+        email: string;
+      };
+    }
   }
 }
+
+export {};
